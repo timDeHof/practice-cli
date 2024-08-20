3f0d8ca1b884c3d38f378b018bb36ba4b2c567e4
timDeHof <ttdehof@gmail.com>
2024-08-20 10:58:48 -0400

    feat(cli): add commander dependency and initialize CLI program structure The commander library is added to facilitate the creation of a command-line interface (CLI) for the application. The initial setup includes defining the program's name, description, and version, which lays the groundwork for further CLI functionality.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+	.name("practice-cli")
+	.description("practice writing CLIs")
+	.version("0.0.0");
+
 const hello = (message: string) => {};
 
 console.log("WOW");
